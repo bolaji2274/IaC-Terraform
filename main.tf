a1e884a85fad6ad8ee72daeeea5fb77bdf2b6f47
Bolaji Hammed <hammedbolajihammed@gmail.com>
2023-07-04 01:43:51 +0000

    file added

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0715c1897453cabd1"
   instance_type = "t2.micro"
-  provisioner "local-exec" {
-    command = "echo The server's IP address is ${self.private_ip}"
+
+  tags = {
+    Name = "terraform-web"
   }
 }
