7c65e2df68b44cd2d9235532c42462c1dc564b71
Bolaji Hammed <hammedbolajihammed@gmail.com>
2023-07-04 03:25:16 +0000

    file added for autoscalling

---
+++ web-server-cluster/main.tf
@@ -0,0 +1,23 @@
+resource "aws_launch_configuration" "web" {
+  image_id = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello WebServer" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF 
+}
+
+resource "aws_autoscaling_group" "web" {
+  launch_configuration = aws_launch_configuration.web.name
+  min_size = 2
+  max_size = 5
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-web"
+    propagate_at_launch = true
+  }
+}
